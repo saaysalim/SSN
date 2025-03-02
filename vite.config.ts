e827d075d06513818958f7a39edc6a425a242dad
Salim Saay <72768220+saaysalim@users.noreply.github.com>
2025-03-02 10:03:44 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SSN", // Ensure this is correct based on your deployment path
+  base: "/SSN/src/Pages/Home", // Ensure this is correct based on your deployment path
 });
